4a0f5f89a4a2d1e46c8ea4c0cde62a45ab54a280
unknown <anhtuphamxt@gamil.com>
2021-07-15 16:04:01 +0700

    Initial commit

---
+++ dist/main.d.ts
@@ -0,0 +1,4 @@
+declare let fibonacci: number[];
+declare let sum: number;
+declare let count: number;
+declare function addNumberFibonacci(totalCount: number): void;
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+let fibonacci=[0,1];
+let sum =1;
+let count = 2;
+function addNumberFibonacci(totalCount: number) : void {
+    let n1=fibonacci.length-1;
+    let n2 = fibonacci.length-2;
+    let n = fibonacci[n1]+fibonacci[n2];
+    console.log(n+",");
+    sum += n;
+    fibonacci.push(n);
+    count++;
+    if(count<totalCount){
+        addNumberFibonacci(totalCount);
+    }else {
+        console.log("sum -->",sum);
+    }
+
+}
+
+addNumberFibonacci(100);
